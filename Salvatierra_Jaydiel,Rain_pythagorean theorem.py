daa0c88b7d6aca53be6c7ff9addaab6500d0084c
jssalvatierra <jssalvatierra@carc.pshs.edu.ph>
2026-09-08 16:55:34 +0800

    Add files via upload

---
+++ Salvatierra_Jaydiel,Rain_pythagorean theorem.py
@@ -0,0 +1,18 @@
+#PYTHON
+
+#Finding the hypotenuse
+
+import math
+
+print("YO we finding the hypotenuse or sum of the triangle")
+
+a = int(input("Enter a number: "))
+#For the first side of a triangle
+b = int(input("Enter b number: "))
+#For the second side of a triangle
+hypotenuse = math.sqrt(a^2 + b^2)
+
+print(f"The hypotenuse is {hypotenuse}")
+
+#Jaydiel Rain S. Salvatierra
+#8 Adelfa
